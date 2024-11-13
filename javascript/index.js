f090cf59c00ec903591a759b3936f72215cc3e1a
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-13 17:20:00 +0100

    Made a connection to the API in the function challengesAPI(), started working on a new function that will try to generate rooms based on data from API

--- javascript/index.js
+++ javascript/index.js
@@ -11,14 +11,28 @@ overlayBlur.addEventListener("click", hideMenu);
 
 //Menu functions.
 function showMenu() {
-    navigationMenu.classList.add("overlay");
-    overlayBlur.classList.add("active");
-    document.body.style.overflow = "hidden";
-};
+  navigationMenu.classList.add("overlay");
+  overlayBlur.classList.add("active");
+  document.body.style.overflow = "hidden";
+}
 
 function hideMenu() {
-    navigationMenu.classList.remove("overlay");
-    overlayBlur.classList.remove("active");
-    document.body.style.overflow = "";
-};
+  navigationMenu.classList.remove("overlay");
+  overlayBlur.classList.remove("active");
+  document.body.style.overflow = "";
+}
 
+//Testing out to connect to the API
+
+async function challengesApi() {
+  const response = await fetch(
+    "https://lernia-sjj-assignments.vercel.app/api/challenges"
+  );
+  const data = await response.json();
+  return data;
+}
+
+async function generateRoom() {
+  const roomData = await challengesApi();
+  
+}
