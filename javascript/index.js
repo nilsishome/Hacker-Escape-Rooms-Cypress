8492a0186e5f31029a395f238a11c42b4ca0bd18
hungryhippoafk <helsing1993@hotmail.com>
2024-11-17 21:20:13 +0100

    Added some JavaScript to be able to close the Modal 2

--- javascript/index.js
+++ javascript/index.js
@@ -22,3 +22,16 @@ function hideMenu() {
     document.body.style.overflow = "";
 };
 
+
+
+//JS for the modal, basic for closing to be able to work 
+const Bookroom_modal = document.querySelector(".Bookroom_modal");
+const overlay = document.querySelector(".overlay");
+
+// Close the modal when clicking on the overlay
+overlay.addEventListener("click", closeMenu);
+
+function closeMenu() {
+   Bookroom_modal.style.display = "none"; // Hides the modal
+   overlay.style.display="none"; // hides the overlay
+}
