d5a7b27aff47b1a655d26f984ce83ee17389d454
ViktorNoskire <vikkerinho@gmail.com>
2024-11-20 23:34:06 +0100

    The function to add eventlisteners on type filter buttons. Not yet added functionality for filtering on-site cards

---
+++ javascript/type_filter.js
@@ -0,0 +1,28 @@
+export default function filterType() {
+    const onlineChallenges = document.querySelector("#online-challenges");
+
+    // Checking if filter button can be worked with
+    if (onlineChallenges) {
+        // Add event listener on online challenges filter button
+        onlineChallenges.addEventListener("change", () => {
+            // put all DOM elements with class challenges__room in one variable
+            const allChallenges = document.querySelectorAll(".challenges__room");
+            // Go through that variable
+            allChallenges.forEach((challenge) => {
+                // If button has been clicked already this will change 
+                // back all the on site elements to show again
+                if (challenge.style.display === "none") {
+                    challenge.style.display = "";
+                } 
+                //
+                else {
+                    if (challenge.getAttribute("class") === "challenges__room online") {
+                        challenge.style.display = "";
+                    } else {
+                        challenge.style.display = 'none';
+                    }
+                }
+            })
+        });
+    }
+};
