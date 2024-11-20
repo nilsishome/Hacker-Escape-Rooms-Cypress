4962275b3b3af79b5e9ea064dd4d1c29e93ae554
nilsishome <nilsnyberg24@gmail.com>
2024-11-20 19:35:31 +0100

    Changed minimum star-value in challenge.html to 0, and made progress on rating filter (still buggy)

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,37 +1,58 @@
+/*
+ *  [ ] Make rateInput.values null or undefined when page reload.
+ *  [x] Make rateInput.values go below value = 1.
+ */
+
 // Creates a function that filters the room cards by rating
 export async function filterByRating() {
-    // Need the rating values from API
-    const response = await fetch(
-        "https://lernia-sjj-assignments.vercel.app/api/challenges"
-    );
-    const data = await response.json();
+  // Need the rating values from API
+  const response = await fetch(
+    "https://lernia-sjj-assignments.vercel.app/api/challenges"
+  );
+  const data = await response.json();
 
-    // firstRate is the value of the first rating
-    const firstRating = document.querySelector(".filter__first-rate");
-    const firstRate = firstRating.querySelector("input[type='radio']:checked");
+  const firstRating = document.querySelector(".filter__first-rate");
+  const secondRating = document.querySelector(".filter__second-rate");
 
-    // secondRate is the value of the second rating
-    const secondRating = document.querySelector(".filter__second-rate");
-    const secondRate = secondRating.querySelector("input[type='radio']:checked");
+  // allChallenges is representing each room, and rateInput is the absolute first radiobox input on the page
+  const allChallenges = document.querySelectorAll(".challenges__room");
+  const rateInput = document.querySelectorAll("input[type='radio']");
 
-    const allChallenges = document.querySelectorAll(".challenges__room");
-
-    if (firstRate && secondRate) {
-        // i variable for looping the rating values
-        let i = 0;
+  // For loop that iterates through ALL starInputs, instead of only the first one
+  for (let i = 0; i < rateInput.length; i++) {
+    rateInput[i].addEventListener("input", () => {
+      // firstRate is the value of the first rateInput
+      const firstRate = firstRating.querySelector(
+        "input[type='radio']:checked"
+      );
+      // secondRate is the value of the second rateInput
+      const secondRate = secondRating.querySelector(
+        "input[type='radio']:checked"
+      );
+      console.log(firstRate.value + "\n" + secondRate.value);
+      
+      // Conditional statement that makes sure the starInputs are active
+      if (firstRate && secondRate) {
+        // j variable for looping the rating values
+        let j = 0;
         allChallenges.forEach((challenge) => {
-            // Implement API object inside DOM-elements
-            challenge.domObject = data.challenges[i];
-            // Make a variable for each rating value in the DOM-elements
-            const challengeRating = challenge.domObject.rating;
-            console.log(challengeRating);
-            i++;
-            if (challengeRating >= firstRate.value && challengeRating <= secondRate.value) {
-                challenge.style.display = "";
-            }
-            else {
-                challenge.style.display = "none";
-            }
+          // Implement API object inside DOM-elements
+          challenge.domObject = data.challenges[j];
+          // Make a variable for each rating value in the DOM-elements
+          const challengeRating = challenge.domObject.rating;
+          console.log("HELLOOOO");
+          j++;
+          // Another conditional statement that makes sure the rateInput values are correct
+          if (
+            challengeRating >= firstRate.value &&
+            challengeRating <= secondRate.value
+          ) {
+            challenge.style.display = "";
+          } else {
+            challenge.style.display = "none";
+          }
         });
-    }
+      }
+    });
+  }
 }
