1b4b94adca1a196082a8d76a2ac4b1cad0822c0b
hungryhippoafk <helsing1993@hotmail.com>
2024-11-20 22:27:18 +0100

    Reworked js code for api and fixed html code

--- javascript/bookroom2.js
+++ javascript/bookroom2.js
@@ -6,43 +6,54 @@ formEl.addEventListener("submit", async event => {
     event.preventDefault(); //prevents reloading the page when submit
     
 
-    const formData = new FormData(formEl);
-    const data = Object.fromEntries(formData);
+    const data = new FormData(formEl);
+    const payload = new URLSearchParams(data);
+
+    //const data = Object.fromEntries(formData);//
+    console.log([...payload]);
 
     
-    const name = document.getElementById("name-text").value;
-    const email = document.getElementById("email-text").value;
-    const time = document.getElementById("time_options").value;
-    const participants = document.getElementById("participants_number").value;
+
+    
+    const userInput_name = document.getElementById("name-text").value;
+    const userInput_email = document.getElementById("email-text").value;
+    const time_input = document.getElementById("time_options").value;
+    const userInput_participants = document.getElementById("participants_number").value;
 
 
-        console.log("Name:", name);
-        console.log("Email:", email);
-        console.log("Time:", time);
-        console.log("Participants_number", participants);
+    const userInput = {
+      challenge: 12 ,
+      name: userInput_name,
+      email: userInput_email,
+      date: time_input, 
+      time: new Date().toISOString(),
+      participants: parseInt(userInput_participants,10),
+    };
+    console.log("user input object:",userInput);
+
+    fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
+      method: 'POST',
+      
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInput),  // Sending data in JSON format
+    })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+
+  
+        console.log("Name:", userInput_name);
+        console.log("Email:", userInput_email);
+        console.log("Time:", new Date().toISOString());
+        console.log("Participants_number", userInput_participants);
    });
 
    
 
 
-   const data = {
-    challenge: 12,
-    name: "text",
-    email: "email",
-    date: "2022-12-12",
-    time: "time",
-    participants: 6,
-  };
-   
-   
-   fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),  // Sending data in JSON format
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch(error => console.error('Error:', error));
+  
+  
 
+  
