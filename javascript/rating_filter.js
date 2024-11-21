6edbe06219ef9fffb04350c3d6aae1bd39a6003c
nilsishome <nilsnyberg24@gmail.com>
2024-11-22 00:28:22 +0100

    Filter by rating works now with fa-stars

--- javascript/rating_filter.js
+++ javascript/rating_filter.js
@@ -1,67 +1,71 @@
-/*
- * [ ] Make rating checkboxes act like radio selection, only one at a time
- */
-
 // Creates a function that filters the room cards by rating
 export async function filterByRating() {
-
-  const filterRating = document.querySelector(".filter__rating");
+  // These variables are for identifying and using the stars and their values later
   const firstRating = document.querySelector(".filter__first-rate");
   const secondRating = document.querySelector(".filter__second-rate");
+  const firstStars = firstRating.querySelectorAll("i");
+  const secondStars = secondRating.querySelectorAll("i");
 
-  const rateInput = filterRating.querySelectorAll("input[type='checkbox']");
+  // The ratings default values
+  let firstRate = 0;
+  let secondRate = 5;
 
-  rateInput.forEach((input) => {
-    input.addEventListener("input", () => {
-      // firstRate is the value of the first rateInput
-      const firstRate = firstRating.querySelector(
-        "input[type='checkbox']:checked"
-      );
-      // secondRate is the value of the second rateInput
-      const secondRate = secondRating.querySelector(
-        "input[type='checkbox']:checked"
-      );
-
-      function selectOnlyThis(id) {
-        for (var i = 1;i <= 4; i++)
-        {
-            input[i].checked = false;
-        }
-        document.getElementById(id).checked = true;
-      }
-
-      input.addEventListener("click", () => {
-        selectOnlyThis(input);
-      });
-
-      console.log(firstRate.value + "\n" + secondRate.value);
-
-      // allChallenges is representing each room
-      const allChallenges = document.querySelectorAll(".challenges__room");
-      
-      // Conditional statement that makes sure the starInputs are active
-      if (firstRate && secondRate) {
-        allChallenges.forEach((challenge) => {
-
-          // Targets the rating value of the cards rating
-          const rating = challenge.rating;
-
-          // Another conditional statement that makes sure the rateInput values are correct
-          if (
-            rating >= firstRate.value &&
-            rating <= secondRate.value
-          ) {
-            challenge.style.display = "";
+  if (firstStars && secondStars) {
+    // This is the value and style transformation of the first stars
+    firstStars.forEach((star, idx1) => {
+      star.addEventListener("click", () => {
+        firstRate = idx1 + 1;
+        if (idx1 === 0) firstRate = idx1;
+        firstStars.forEach((star, idx2) => {
+          if (idx1 >= idx2) {
+            star.classList.remove("fa-regular");
+            star.classList.add("fa-solid");
           } else {
-            challenge.style.display = "none";
+            star.classList.remove("fa-solid");
+            star.classList.add("fa-regular");
           }
         });
-      }
+        updateFilter();
+      });
     });
-  });
+
+    // This is the value and style transformation of the second stars
+    secondStars.forEach((star, idx1) => {
+      star.addEventListener("click", () => {
+        secondRate = idx1 + 1;
+        secondStars.forEach((star, idx2) => {
+          if (idx1 >= idx2) {
+            star.classList.remove("fa-regular");
+            star.classList.add("fa-solid");
+          } else {
+            star.classList.remove("fa-solid");
+            star.classList.add("fa-regular");
+          }
+        });
+        updateFilter();
+      });
+    });
+
+    function updateFilter() {
+      // allChallenges is representing each room
+      const allChallenges = document.querySelectorAll(".challenges__room");
+
+      allChallenges.forEach((challenge) => {
+        // Targets the rating value of the cards rating
+        const rating = challenge.rating;
+
+        // A conditional statement that makes sure the rateInput values are correct
+        if (rating >= firstRate && rating <= secondRate) {
+          challenge.style.display = "";
+        } else {
+          challenge.style.display = "none";
+        }
+      });
+    }
+  }
 }
 
-// This function resets the checkboxes and radioboxes in probably all browsers 
+// This function resets the checkboxes and radioboxes in probably all browsers
 // (I've only seen this 'problem in Firefox')
 export function resetForm() {
   const filterForm = document.querySelector(".filter__form");
