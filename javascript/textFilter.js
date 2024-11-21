97e73d084a6a6968b7951746b1da0b3490fb2adb
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-21 10:45:19 +0100

    Added a message with JS when not any challenges gets matchen by the filter, added some SCSS styling for the message

--- javascript/textFilter.js
+++ javascript/textFilter.js
@@ -4,12 +4,19 @@ export function filterByText() {
   const searchInput = document.querySelector("#filter__text");
   //If the searchinput was found, the code runs.
   if (searchInput) {
+    //Create <p> element for the message if no challanges are found by the filter.
+    const container = document.querySelector("#challenges__container");
+    const message = document.createElement("p");
+    message.textContent = "No matching challenges";
+    message.classList.add("filter__message");
     //added eventlistener that runs everytime the user inputs in the searchfield.
     searchInput.addEventListener("input", () => {
       //Converts the users text to lowercase to avoind problems with upper / lower case from the user.
       const searchText = searchInput.value.toLowerCase();
-      //Targets the div that has all the challanges generated inside it.
+      //Get all the challenges inside the challenges__container
       const allChallenges = document.querySelectorAll(".challenges__room");
+      //Sets filteredChallenges to false.
+      let filteredChallenges = false;
 
       //ForEach that goes through one room at a time
       allChallenges.forEach((challenge) => {
@@ -21,11 +28,20 @@ export function filterByText() {
         if (title.includes(searchText) || description.includes(searchText)) {
           //If a room has matching text, it gets displayed.
           challenge.style.display = "";
+          //filteredChallenges becomes true, if a room is shown by the users searchtext.
+          filteredChallenges = true;
         } else {
           //If a room does NOT have matching text, it wont be displayed.
           challenge.style.display = "none";
         }
       });
+      //If no challenges matched the user text, the message is shown.
+      if (!filteredChallenges) {
+        container.appendChild(message);
+      } else {
+        //Or if a found a match from the searchtext, the message is removed.
+        message.remove();
+      }
     });
   }
 }
