b246e74f419663455eae16abab72b78b424b4a3f
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-22 21:12:27 +0100

    started working on a foreach loop, far from done. Last commit for the day, will continue tomorrow.

--- javascript/filterByLabel.js
+++ javascript/filterByLabel.js
@@ -1,4 +1,14 @@
 export function filterByLabel() {
     //takes all filter buttons from .filter__tag.
-    const labelButton = document.querySelectorAll(".filter__tag button")
+    const labelButton = document.querySelectorAll(".filter__tag button");
+    //Empry array to store the labels that are active
+    let activeLabels = [];
+
+    labelButton.forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            button.classList.toggle("active");
+        })
+    })
 }
