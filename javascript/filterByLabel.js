616378b746d0270f9d0a394884d88ad5dccaee31
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-24 19:55:36 +0100

    added a const clikedLabel to store the buttons label to lowercase, thinking this might be good for future to match the labels from the API

--- javascript/filterByLabel.js
+++ javascript/filterByLabel.js
@@ -10,10 +10,14 @@ export function filterByLabel() {
     //When the user clicks on our labels, the filter runs.
     filterButtons.forEach(filterButton => {
         filterButton.addEventListener("click", (e) => {
+            //Prevents the site from reloading when the labels buttons are clicked.
             e.preventDefault();
             
-
+            //Added a toggle to style with SCSS when the label is "active"
             filterButton.classList.toggle("active");
+            //Gets the label on the specific label that has been clicked.
+            const clickedLabel = filterButton.textContent.toLocaleLowerCase();
+            console.log(clickedLabel);
         })
     })
 }
