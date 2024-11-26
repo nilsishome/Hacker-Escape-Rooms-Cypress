987be22818b3666757cf26336831a5b513d30cc0
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-26 12:40:21 +0100

    started working on navigation from index.html to challenges.html for all the buttons

---
+++ javascript/navigation.js
@@ -0,0 +1,6 @@
+function navigationFilter() {
+    const onlineButton = document.querySelector(".introduction__button--online");
+    const onsiteButton = document.querySelector(".introduction__button--onsite");
+    const bookOnsiteButton = document.querySelector(".play__button--book");
+    const allChallengesButton = document.querySelector(".play__button--challenges");
+}
