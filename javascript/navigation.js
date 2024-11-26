abe441463917198ab70bdaaf520f2b9cd9edb8c2
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-26 17:39:12 +0100

    Used querySelectorAll instead since they all to the same thing.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,37 +1,21 @@
 export function navigation() {
   //Getting all the buttons on index.html
-  const onlineButton = document.querySelector(".introduction__button--online");
-  const onlineButtonTwo = document.querySelector(".content__navigation-link");
-  const onsiteButton = document.querySelector(".introduction__button--onsite");
-  const onsiteButtonTwo = document.querySelector(".content__navigation-button");
-  const bookOnsiteButton = document.querySelector(".play__button--book");
-  const allChallengesButton = document.querySelector(
-    ".play__button--challenges"
+  const indexButtonNavigation = document.querySelectorAll(
+    ".introduction__button--online, .content__navigation-link, .introduction__button--onsite, .content__navigation-button, .play__button--book, .play__button--challenges"
   );
-
   //Getting logo and title to navigate to "Homepage"
   const headerNavigation = document.querySelectorAll(
     ".header__logo, .header__title"
   );
-
-  //Put all buttons in an array.
-  const allButtons = [
-    onlineButton,
-    onsiteButton,
-    bookOnsiteButton,
-    allChallengesButton,
-    onlineButtonTwo,
-    onsiteButtonTwo,
-  ];
   //For each button an eventListener gets added that on click navigates to challenges.html.
-  allButtons.forEach((button) => {
+  indexButtonNavigation.forEach((button) => {
     if (button) {
       button.addEventListener("click", () => {
         window.location.href = "./challenges.html";
       });
     }
   });
-
+  //foreach to add eventlistener to logo and logotitle and onclick navigate to index.html
   headerNavigation.forEach((logo) => {
     if (logo) {
       logo.addEventListener("click", () => {
