f15962731bca2be3b4f438f8593432b8ca903a36
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-27 20:22:09 +0100

    Removed the logic for the mobile menu overlay from index.js and added in to the navigation.js file.

--- javascript/index.js
+++ javascript/index.js
@@ -4,29 +4,6 @@ import filterType from "./type_filter.js";
 import { filterByLabel } from "./filterByLabel.js";
 import { navigation } from "./navigation.js";
 
-//variables.
-const menuButton = document.querySelector(".header__menu-button");
-const closeButton = document.querySelector(".header__navigation-close");
-const navigationMenu = document.querySelector(".header__navigation");
-const overlayBlur = document.querySelector(".overlay__blur");
-
-//event-listeners.
-menuButton.addEventListener("click", showMenu);
-closeButton.addEventListener("click", hideMenu);
-overlayBlur.addEventListener("click", hideMenu);
-
-//Menu functions.
-function showMenu() {
-  navigationMenu.classList.add("overlay");
-  overlayBlur.classList.add("active");
-  document.body.style.overflow = "hidden";
-}
-
-function hideMenu() {
-  navigationMenu.classList.remove("overlay");
-  overlayBlur.classList.remove("active");
-  document.body.style.overflow = "";
-}
 //Connection to API, this function can now be called wherever we need it.
 async function challengesApi() {
   const response = await fetch(
