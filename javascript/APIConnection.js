9f231a54d7e76bc73d1b46aa0a2dccb2b8e95cbd
Rikard Engström <rikardengstrom89@gmail.com>
2024-11-29 11:20:47 +0100

    Rikard and viktor have been running tests on filter functionality.

--- javascript/APIConnection.js
+++ javascript/APIConnection.js
@@ -1,7 +1,7 @@
 export async function challengesApi() {
-    const response = await fetch(
-      "https://lernia-sjj-assignments.vercel.app/api/challenges"
-    );
-    const data = await response.json();
-    return data;
-  }
+  const response = await fetch(
+    "https://lernia-sjj-assignments.vercel.app/api/challenges"
+  );
+  const data = await response.json();
+  return data;
+}
